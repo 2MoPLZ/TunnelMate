46729a13a0b17ca7bde871c192ee5e66df8e61c7
kimmin <jangofett@naver.com>
2025-04-29 12:15:45 +0900

    feat: lcd 드라이버 기능 구현

---
+++ TC275/out/obj/illd/src/IfxSrc_cfg.c.d
@@ -0,0 +1,5 @@
+obj/illd/src/IfxSrc_cfg.c.o: \
+ /cygdrive/c/Users/USER/Desktop/WORKSP~1/TEAMPR~1/TUNNEL~1/TC275/illd/src/IfxSrc_cfg.c \
+ /cygdrive/c/Users/USER/Desktop/WORKSP~1/TEAMPR~1/TUNNEL~1/TC275/illd/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.h
+
+/cygdrive/c/Users/USER/Desktop/WORKSP~1/TEAMPR~1/TUNNEL~1/TC275/illd/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.h:
